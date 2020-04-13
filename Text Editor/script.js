a21068377787a3f3bdc977ad8f2063ac579dff9a
saywurdson <34385409+saywurdson@users.noreply.github.com>
2020-04-12 20:56:10 -0700

    update js file

--- Text Editor/script.js
+++ Text Editor/script.js
@@ -8,9 +8,8 @@
 
 /**
  * Update the output text as a user types in the textarea
- * HINT: Use the onkeydown function inside HTML
  */
-function updateText(){
+updateText = () => {
   let text = document.getElementById('text-input').value;
   document.getElementById('text-output').innerText = text; // mirrors the text from the input box into the formatted text box
 }
@@ -18,7 +17,7 @@ function updateText(){
 /**
  * Toggle the bold class for the output text
  */
-function makeBold(elem){
+makeBold = (elem) => {
   elem.classList.toggle('active'); // Toggles Bold button on and off
   document.getElementById('text-output').classList.toggle('bold'); // Makes formatted text bold when button is toggled on
 }
@@ -26,7 +25,7 @@ function makeBold(elem){
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem){
+makeItalic = (elem) => {
   elem.classList.toggle('active'); // Toggles Italic button on and off
   document.getElementById('text-output').classList.toggle('italic'); // Makes formatted text italic when button is toggled on
 }
@@ -34,7 +33,7 @@ function makeItalic(elem){
 /**
  * Toggle the underline class for the output text
  */
-function makeUnderline(elem){
+makeUnderline = (elem) => {
   elem.classList.toggle('active'); // Toggles Italic button on and off
   document.getElementById('text-output').classList.toggle('underline'); // Makes formatted text underlined when button is toggled on
 }
@@ -43,12 +42,11 @@ function makeUnderline(elem){
  * Toggle the style textAlign attribute
  * Toggle the active state for the align butttons
  */
-function alignText(elem, alignType){
+alignText = (elem, alignType) => {
   elem.classList.toggle('active'); // Toggles aling button on and off
   document.getElementById('text-output').style.textAlign = alignType; // Changes the style of the text to the align type
-  let buttonsList = document.getElementsByClassName('align')
-  for(let i = 0; i < buttonsList.length; i ++){
-    buttonsList[i].classList.remove('active'); // For loop that cyles through the other align styles and disables the ones that aren't selected 
+  let buttonsList = document.getElementsByClassName('align');
+  for(let button of buttonsList) {button.classList.remove('active');
   }
   elem.classList.add('active'); // Only allows the button that was clicked to be active
 }
