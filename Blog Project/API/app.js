e8a0fe3e6b19fc2d5bdaa486842662961d23a9cb
saywurdson <34385409+saywurdson@users.noreply.github.com>
2020-06-15 17:34:24 -0700

    finished visuals incomplete API connect

--- Blog Project/API/app.js
+++ Blog Project/API/app.js
@@ -4,6 +4,26 @@ const app = express();
 const Post = require("./api/models/posts)");
 const postsData = new Post();
 
+var multer = require("multer")
+var storage = multer.diskStorage ({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+var upload = multer({storage: "uploads/"})
+
+const getExt = (mimeType) => {
+    switch(mimeType) {
+        case "image/png":
+            return ".png";
+        case "image/jpeg":
+            return ".jpeg";
+    }
+}
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
@@ -24,6 +44,18 @@ app.get("/api/posts/:post_id", (req, res) => {
     } else {
         res.status(404).send("Not Found");
     }
+})
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post-image": req.file.path,
+         "added-date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send(newPost);
 })
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"))
--- Blog Project/App/js/post.js
+++ Blog Project/App/js/post.js
@@ -1,8 +1,5 @@
-/**
- * TODO (Together): Create getPostIdParam to get the id of the post to use in the request later
- * TODO: Complete getPost function to get post data from API
- * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
- */
+const { url } = require("inspector");
+const { response } = require("express");
 
 const API_URL = "http://localhost:3000/api/posts/";
 const API_BASE_URL = "http://localhost:3000/";
@@ -11,11 +8,32 @@ window.onload = () => {
     getPost();
 }
 
+const getPostIdParam = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id");
+}
+
 const getPost = () => {
-    // CODE GOES HERE
+    const postID = getPostIdParam();
+    const url = `${API_URL}${postId}`;
+    fetch(url, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        buildPost(data);
+    })
+
 }
 
 const buildPost = (data) => {
-    // HINT: Convert the date number to a Date string 
+    const postDate = new Date(parseInt(data.added_date)).toDateString();
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+    document.querySelector("header").style.backgroundImage = `url(${postImage})`;
+    document.getElementById("individual-post-title").innerText = data.title;
+    document.getElementById("individual-post-date").innerText = `Published on ${postDate}`;
+    document.getElementById("individual-post-content").innerText = data.content;
 }
+
 
