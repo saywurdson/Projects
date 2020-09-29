74dfe3a20b0e86bc3bb620567ef31a0ff575fc47
saywurdson <kagyeman@hotmail.com>
2020-09-29 07:36:06 +0000

    Continue age game

--- Your Age In Days/script.js
+++ Your Age In Days/script.js
@@ -1,1 +1,9 @@
-alert('hi');
+function ageInDays() {
+  var birthYear = prompt('What Year Were You Born?');
+  var theAgeInDays = (2020 - birthYear) * 365;
+  var h1 = document.createElement('h1');
+  var textAnswer = document.createTextNode('You Are ' + ageInDays + ' days old');
+  h1.setAttribute('id', "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById('flex-box-result').appendChild(h1);
+}
