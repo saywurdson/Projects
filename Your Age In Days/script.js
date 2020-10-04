db154cf0c1ae72ba16f6281d4a81df3aad4c8a97
saywurdson <kagyeman@hotmail.com>
2020-10-04 07:58:33 +0000

    Finished Your Age in Days mini project

--- Your Age In Days/script.js
+++
@@ -1,9 +0,0 @@
-function ageInDays() {
-  var birthYear = prompt('What Year Were You Born?');
-  var theAgeInDays = (2020 - birthYear) * 365;
-  var h1 = document.createElement('h1');
-  var textAnswer = document.createTextNode('You Are ' + ageInDays + ' days old');
-  h1.setAttribute('id', "ageInDays");
-  h1.appendChild(textAnswer);
-  document.getElementById('flex-box-result').appendChild(h1);
-}
